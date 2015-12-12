d197014bacb483b7738e42eeb455cf15e7115a5b
Holag <holag147@googlemail.com>
2015-12-12 17:45:55 +0100

    Finaly

---
+++ Bananenbrot/Assets/Code/Ball.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+ var rb: Rigidbody2D;
+
+function Start () {
+  var rn = Random.Range(0,2);
+  rb = GetComponent.<Rigidbody2D>();
+  if (rn <= 0.5){
+    rb.AddForce(new Vector2 (30, 10));
+  }else{
+  	rb.AddForce(new Vector2 (-30, -10));
+  }
+}
+
+function OnCollisionEnter2D (colInfo : Collision2D) {
+Debug.Log("Nudelsuppe");
+	if(colInfo.collider.tag == "Player"){
+		var velY = rb.velocity.y;
+		velY = velY/2 + colInfo.collider.GetComponent.<Rigidbody2D>().velocity.y/3;
+	}
+}
---
+++ Bananenbrot/Assets/Code/Player.js
@@ -0,0 +1,27 @@
+﻿var moveUp : KeyCode;
+var moveDown : KeyCode;
+
+var speed : float = 10;
+
+function Start() {
+  rb = GetComponent.<Rigidbody2D>();
+}
+
+function Update () {
+var rb = GetComponent.<Rigidbody2D>();
+	if(Input.GetKey(moveUp))
+	{
+		rb.velocity.y = speed;
+	}
+	else if(Input.GetKey(moveDown))
+	{
+		rb.velocity.y = speed *-1;
+	}
+	else
+	{
+		rb.velocity.y = 0;
+	}
+	
+	rb.velocity.x = 0;
+
+}
